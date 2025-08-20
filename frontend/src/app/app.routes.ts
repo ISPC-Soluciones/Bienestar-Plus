7668037f8cf41c2353f40173b5c0d5b34a2f2a19
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-08-20 18:57:12 -0300

    nueva feature - Sección Perfil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Perfil } from './perfil/perfil';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'perfil', component: Perfil },
+  { path: '', redirectTo: '/perfil', pathMatch: 'full' },
+  { path: '**', redirectTo: 'perfil' },
+];
---
+++ frontend/src/app/perfil/perfil.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './perfil.html',
+  styleUrl: './perfil.css',
+})
+export class Perfil {
+  usuario = {
+    nombre: 'Cosme Fulanito',
+    email: 'cosmefulanito@ispc.com.ar',
+    progreso: '¡Felicidades has reducido tu estrés diario en un 50%!',
+    foto: 'assets/usuario.jpg',
+    grafico: 'assets/grafico_usuario_metricas.jpg',
+  };
+}
