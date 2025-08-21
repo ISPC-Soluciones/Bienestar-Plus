42a3f90bb6243ee39b6a18bde437c1d18710d89d
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-08-21 01:39:25 -0300

    nuevo componente habitos creado

---
+++ frontend/src/app/habitos/habitos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habitos',
+  imports: [],
+  templateUrl: './habitos.html',
+  styleUrl: './habitos.css'
+})
+export class Habitos {
+
+}
