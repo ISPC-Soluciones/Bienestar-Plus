885fcf71848d3baf6e941a7a4d9b09bedd42b038
nahir1009 <nahir1009@gmail.com>
2025-08-21 18:42:31 -0300

    fix creo componente registro html

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { Perfil } from './perfil/perfil';
 import { Habitos } from './habitos/habitos';
-import { Login } from './login/login';
+import { Registro } from './registro/registro'; 
 // import { Home } from './';
 // import { NotFound } from './';
 
 export const routes: Routes = [
   { path: 'perfil', component: Perfil },
   { path: 'habitos', component: Habitos },
-  {path: 'login',component:Login }
+  {path: 'registro',component: Registro }
   // { path: '', component: Home  },
   // { path: '**', component: NotFound },
 ];
