6a8beace06b58e95dfb62a8204440b9a33537079
nahir1009 <nahir1009@gmail.com>
2025-08-21 18:22:23 -0300

    fix: agrego componente login y inyecto html

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { Perfil } from './perfil/perfil';
 import { Habitos } from './habitos/habitos';
+import { Login } from './login/login';
 // import { Home } from './';
 // import { NotFound } from './';
 
 export const routes: Routes = [
   { path: 'perfil', component: Perfil },
   { path: 'habitos', component: Habitos },
+  {path: 'login',component:Login }
   // { path: '', component: Home  },
   // { path: '**', component: NotFound },
 ];
