196fc31ea7c1f5f232f460dc2fa347a9d1ff1c59
Eric-Heredia <ericvheredia@gmail.com>
2025-08-23 12:56:20 -0300

    fix: regreso a la parte superior de la página

--- frontend/src/app/footer/footer.ts
+++ frontend/src/app/footer/footer.ts
@@ -9,4 +9,19 @@ import { RouterModule } from '@angular/router';
 })
 export class Footer {
 
+  /**
+   * Desplaza la ventana del navegador a la parte superior de la página.
+   * @param event El evento de clic.
+   */
+  scrollToTop(event: MouseEvent): void {
+    // Previene la acción por defecto del enlace si no es un routerLink
+    if (event.target instanceof HTMLAnchorElement && event.target.getAttribute('routerLink') === null) {
+      event.preventDefault();
+    }
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
