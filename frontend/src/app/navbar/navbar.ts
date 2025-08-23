43460205c215b43899bd79602bc9aac3c57eee74
nahir1009 <nahir1009@gmail.com>
2025-08-23 20:07:21 -0300

    guardo

--- frontend/src/app/navbar/navbar.ts
+++ frontend/src/app/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone:true,
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
