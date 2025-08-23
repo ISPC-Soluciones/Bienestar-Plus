5da878eb6c01ca6ca86d0d2c8a04b18b89d69c06
nahir1009 <nahir1009@gmail.com>
2025-08-22 21:36:02 -0300

    ruting

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Perfil } from './perfil/perfil';
 import { Habitos } from './habitos/habitos';
 import { Home } from './home/home';
+import { Registro } from './registro/registro';
 // import { NotFound } from './';
 
 export const routes: Routes = [
   { path: 'perfil', component: Perfil },
   { path: 'habitos', component: Habitos },
+  {path: 'registro', component: Registro },
   { path: '', component: Home  },
   {path:"", redirectTo:"/home", pathMatch:"full"}
   // { path: '**', component: NotFound },
