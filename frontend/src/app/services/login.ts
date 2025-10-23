d004044f587f3b125d6894c0f52c2e4c829563a9
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-10-23 04:56:02 -0300

    Bugfix: viejo localhost

--- frontend/src/app/services/login.ts
+++ frontend/src/app/services/login.ts
@@ -1,49 +1,45 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs'; 
-import { catchError, map } from 'rxjs/operators'; 
-
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from './../../environments/environment';
 export interface LoginData {
   email: string;
   password: string;
 }
 
-
-export interface Usuario { 
+export interface Usuario {
   id: number;
-  email: string; 
+  email: string;
   nombre: string;
-  password?: string; 
+  password?: string;
 }
-
 
 interface LoginApiResponse {
-    data: {
-        id: number;
-        nombre: string;
-        mail: string; 
-        rol: string;
-    };
-    success: boolean;
-   
+  data: {
+    id: number;
+    nombre: string;
+    mail: string;
+    rol: string;
+  };
+  success: boolean;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://127.0.0.1:8000/login/';
+  private apiUrl = `${environment.backendUrl}/login/`;
 
-  login(loginData: LoginData): Observable<Usuario | null> { 
+  login(loginData: LoginData): Observable<Usuario | null> {
     return this.http.post<LoginApiResponse>(this.apiUrl, loginData).pipe(
-      map(response => {
+      map((response) => {
         if (response?.success && response?.data?.id != null) {
           const usuarioConformado: Usuario = {
             id: response.data.id,
-            email: response.data.mail,   
-            nombre: response.data.nombre
+            email: response.data.mail,
+            nombre: response.data.nombre,
           };
           return usuarioConformado;
         }
@@ -51,15 +47,18 @@ export class LoginService {
       }),
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401 || error.status === 403) {
-          console.warn('LoginService: Credenciales inválidas. Devolviendo null.', error.status);
-          return of(null); 
+          console.warn(
+            'LoginService: Credenciales inválidas. Devolviendo null.',
+            error.status
+          );
+          return of(null);
         }
-        console.error('LoginService: Error grave de conexión o servidor.', error);
+        console.error(
+          'LoginService: Error grave de conexión o servidor.',
+          error
+        );
         return throwError(() => error);
       })
     );
   }
-
-
 }
-
