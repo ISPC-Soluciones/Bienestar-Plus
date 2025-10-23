17b5579b73cb48ececec90268dd7c8a5cde9040a
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-10-23 10:13:37 -0300

    Again

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 1. Construir el Frontend
-npm run build --prefix frontend
-
-# 2. Navegar al Backend, instalar dependencias y recolectar estáticos
-cd backend
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
