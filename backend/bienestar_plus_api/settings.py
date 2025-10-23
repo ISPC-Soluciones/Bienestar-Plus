aa8717c0e4d2beb96c0f54a4bd9fb313b2e777f4
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-10-23 06:30:46 -0300

    Empty Commit

--- backend/bienestar_plus_api/settings.py
+++ backend/bienestar_plus_api/settings.py
@@ -138,7 +138,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static filess (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
