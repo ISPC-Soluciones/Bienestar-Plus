d1b73c05b90edf47787824c9ea552c849a56585c
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-10-23 05:38:04 -0300

    fix wsgi.py PATH

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,12 @@
-# Bienestar-Plus/backend/wsgi.py
-
 import os
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
-# IMPORTANTE: Apunta al settings.py dentro de tu carpeta de proyecto Django
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bienestar_plus_api.settings')
 
 application = get_wsgi_application()
