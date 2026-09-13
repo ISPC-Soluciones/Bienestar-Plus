1fc26c5c08ae2cc630a53698b3137ba844bada93
santyagote <sbustos12345@gmail.com>
2026-09-12 21:10:29 -0300

    refactor(news): consumir noticias desde el backend

--- frontend/src/app/services/noticias.ts
+++ frontend/src/app/services/noticias.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { NoticiasResponse } from '../models/noticia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Noticias {
-  private apiKey = 'b436bd8c4cb54192a8ce45d1557417a1'; 
-  private apiUrl: string; 
+  private readonly apiUrl = `${environment.backendUrl}/api/noticias/`;
 
-  constructor(private http: HttpClient) {
-    const query = 'health diet';
+  constructor(private http: HttpClient) {}
 
-    this.apiUrl = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&language=es&sortBy=publishedAt&apiKey=${this.apiKey}`;
-  }
-
-  getNews(): Observable<any> {
-    console.log("Consultando la URL:", this.apiUrl);
-    return this.http.get(this.apiUrl);
+  getNews(): Observable<NoticiasResponse> {
+    return this.http.get<NoticiasResponse>(this.apiUrl);
   }
 }
