ea21546a1765e82a49dbcb5fd04bcc334cae27df
nahir1009 <nahir1009@gmail.com>
2026-09-20 16:27:50 -0300

    creo archivo routing.py y agrego su configuracion de ruta

--- backend/bienestar_plus_api/asgi.py
+++ backend/bienestar_plus_api/asgi.py
@@ -1,16 +1,26 @@
 """
 ASGI config for bienestar_plus_api project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bienestar_plus_api.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'bienestar_plus_api.settings'
+)
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from bienestar.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
