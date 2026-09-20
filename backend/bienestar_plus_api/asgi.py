8ec90f765f680938227c418b8e68fd7eb636fdc2
nahir1009 <nahir1009@gmail.com>
2026-09-20 18:27:49 -0300

    instalo daphne y agrego a requieriments

--- backend/bienestar_plus_api/asgi.py
+++ backend/bienestar_plus_api/asgi.py
@@ -8,16 +8,20 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
+from bienestar.routing import websocket_urlpatterns
+
+
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
     'bienestar_plus_api.settings'
 )
 
+
 django_asgi_app = get_asgi_application()
 
-from bienestar.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
+
     'http': django_asgi_app,
 
     'websocket': AuthMiddlewareStack(
